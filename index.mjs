23d4fc5490d9899f0fbdcf6dd6611816fa7887b4
jrfl0wrs <jrflower@aggies.ncat.edu>
2024-06-27 14:31:23 -0400

    had to change a few words

--- index.mjs
+++ index.mjs
@@ -4,7 +4,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to my server HOE!');
+  res.send('Welcome to my HOME, Hello world!');
 });
 
 app.listen(port, () => {
